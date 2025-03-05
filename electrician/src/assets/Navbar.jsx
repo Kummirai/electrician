889c5e3f708c65ab3aa080552d196ccbdcd5c481
Kummirai <ajaxmilton@hotmail.com>
2025-03-05 10:49:45 +0200

    Update navbar styles

--- electrician/src/assets/Navbar.jsx
+++ electrician/src/assets/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">Reliable Electrician Services</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
