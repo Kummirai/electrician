29bda4cc29fc5463ce072eae576a4edc3ed26b9f
Kummirai <ajaxmilton@hotmail.com>
2025-03-05 10:02:49 +0200

    Create for component

---
+++ electrician/src/assets/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-8 bg-gray-800 text-white py-4 text-center">
+      &copy; 2025 Reliable Electrician Services. All Rights Reserved.
+    </footer>
+  );
+}
